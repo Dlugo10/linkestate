d53380cc41435777239c8adb6b97d88c6fd030f4
david lugo <davidlugo936@gmail.com>
2021-03-15 10:03:51 -0500

    fixed heart error

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :properties do
+    resources :favorites, only: [:show, :create, :destroy]
      resources :tax_histories, only: [:index, :show, :new, :create, :edit, :update ]
 #     resources :tax_histories, only: [:show, :update, :create]
   end
-  resources :favorites, only: [:index, :show, :create, :destroy]
+  resources :favorites, only: [:index]
 #   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
